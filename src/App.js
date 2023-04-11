833e1c86959fa2400743a967cfbf75190db48821
PKTseng <jk0910948659@gmail.com>
2023-04-12 00:10:32 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// const name = "ken"; //參數渲染
+// const getAge = () => 18; //方法渲染
+// const flag = true; // 三元判斷式
+
+// 来个列表
+const songs = [
+  { id: 1, name: "痴心绝对" },
+  { id: 2, name: "像我这样的人" },
+  { id: 3, name: "南山南" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {songs.map((i) => (
+          <li key={i.id}>{i.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
